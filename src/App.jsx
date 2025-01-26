abdc264be3a1f89779d7a14ce382151ec548a0e9
vedant kulkarni <vedantkulkarni1155@gmail.com>
2025-01-26 23:56:44 +0530

    project setup Done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <h1 className="text-5xl font-extrabold text-white drop-shadow-lg">
+        Welcome to <span className="text-yellow-300">Binge-Watch</span> 🍿🎥
+      </h1>
+    </div>
+  );
+};
+
+export default App;
