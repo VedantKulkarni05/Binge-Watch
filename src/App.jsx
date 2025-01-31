50289e49efb927213576ebf73fd37a19fd3d9691
vedant kulkarni <vedantkulkarni1155@gmail.com>
2025-01-31 19:48:08 +0530

    issue solve ! all working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-      <h1 className="text-5xl font-extrabold text-white drop-shadow-lg">
-        Welcome to <span className="text-yellow-300">Binge-Watch</span> 🍿🎥
-      </h1>
-    </div>
+    <main>
+      <div className="design" />
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You Love,
+            Effortlessly!🎬✨
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   );
 };
 
