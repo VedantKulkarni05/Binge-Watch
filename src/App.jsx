d084951beeb94a761b18b8bf445620a31a4add46
vedant kulkarni <vedantkulkarni1155@gmail.com>
2025-01-31 21:45:43 +0530

    BG image Issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ const App = () => {
 
       <div className="wrapper">
         <header>
+          <img src="./hero-img.png" alt="Hero Banner" />
+
           <h1>
-            Find <span className="text-gradient">Movies</span> You Love,
-            Effortlessly!🎬✨
+            {/*eslint-disable-next-line react/no-unescaped-entities*/}
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
           </h1>
         </header>
         <p>Search</p>
